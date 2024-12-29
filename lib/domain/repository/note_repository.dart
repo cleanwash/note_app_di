85c18ebe2517301f61745953d3213c6a32eabeab
Jayone Lee <kenbugless@gmail.com>
2024-12-29 16:03:09 +0900

    feat:sqflite 설정

--- lib/domain/repository/note_repository.dart
+++ lib/domain/repository/note_repository.dart
@@ -2,8 +2,7 @@ import 'package:note_app_di/domain/model/note.dart';
 
 abstract interface class NoteRepository {
   Future<List<Note>> getNotes();
-
-  Future<Note> getNoteById(int id);
+  Future<Note?> getNoteById(int id);
   Future<void> insertNote(Note note);
   Future<void> updatetNote(Note note);
   Future<void> deleteNote(Note note);
